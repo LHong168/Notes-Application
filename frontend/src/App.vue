6b71d557874de53a6187b19ba7089c60b299ac09
Hong LY <deprohong@gmail.com>
2025-05-07 15:39:27 +0700

    feat(frontend): setup tailwinid amd implement base header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import NoteHeaderView from './features/note/components/NoteHeaderView.vue';
 </script>
 
 <template>
+  <NoteHeaderView />
   <RouterView />
 </template>
