5fb8f5114c850e142c8c1b0f22fb598875d58963
Hong LY <deprohong@gmail.com>
2025-05-07 20:53:11 +0700

    fix update

--- frontend/src/config/data.ts
+++
@@ -1,4 +0,0 @@
-export const LIST_DATA = [
-  { id: 1, title: 'First Note', content: 'This is a note.', createdAt: '2025-05-06' },
-  { id: 2, title: 'Second Note', content: 'Another note.', createdAt: '2025-05-05' }
-]
