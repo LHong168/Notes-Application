be5c6d75147843c4de76e5108058bd54c8df70cf
Hong LY <deprohong@gmail.com>
2025-05-09 00:45:09 +0700

    fix: update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,4 +13,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://lyhong-001-site1.ltempurl.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api'),
+      },
+    },
+  },
 })
